d59ea5919f6f23583ebc1b54929680bf3402ad30
duongnguyen17 <duongthptnt@gmail.com>
2021-08-02 14:16:22 +0700

    socket

--- src/screens/chat/Chat.js
+++ src/screens/chat/Chat.js
@@ -2,59 +2,67 @@ import React, {useState, useEffect, useCallback} from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import {GiftedChat} from 'react-native-gifted-chat';
 import {Button, TextInput} from 'react-native-paper';
-import {logOut, updateUsername} from '../../firebase';
-import auth from '@react-native-firebase/auth';
-import firestore from '@react-native-firebase/firestore';
+// import {logOut, updateUsername} from '../../firebase';
+import io from 'socket.io-client';
+import {urlServer} from '../../constants';
+// import firestore from '@react-native-firebase/firestore';
 const Chat = ({route}) => {
   const {currUser} = route.params;
-  const ref = firestore().collection('room');
+  // const ref = firestore().collection('room');
   const [username, setUsername] = useState('');
   const [messages, setMessages] = useState([]);
-  const [isUpdate, setIsUpdate] = useState(false);
-
+  const socket = io(urlServer);
   useEffect(() => {
-    ref.onSnapshot(querySnapshot => {
-      const list = [];
-      querySnapshot.forEach(doc => {
-        const {_id, text, createdAt, user} = doc.data();
-        list.push({
-          _id,
-          createdAt: createdAt.toDate(),
-          text,
-          user: {_id: user._id},
-        });
-      });
-      console.log(`list`, list);
-      setMessages(
-        list.sort((obj1, obj2) => (obj1.createdAt < obj2.createdAt ? 1 : -1)),
-      );
+    socket.on('chat message', msg => {
+      setMessages([...messages, msg]);
     });
   }, []);
+  // useEffect(() => {
+  //   ref.onSnapshot(querySnapshot => {
+  //     const list = [];
+  //     querySnapshot.forEach(doc => {
+  //       const {_id, text, createdAt, user} = doc.data();
+  //       list.push({
+  //         _id,
+  //         createdAt: createdAt.toDate(),
+  //         text,
+  //         user: {_id: user._id},
+  //       });
+  //     });
+  //     console.log(`list`, list);
+  //     setMessages(
+  //       list.sort((obj1, obj2) => (obj1.createdAt < obj2.createdAt ? 1 : -1)),
+  //     );
+  //   });
+  // }, []);
 
-  const handleUpdate = async () => {
-    await updateUsername(username);
-  };
+  // const handleUpdate = async () => {
+  //   await updateUsername(username);
+  // };
+  const onSend = useCallback((messages = []) => {
+    socket.emit('ping', messages[0]);
+  });
 
-  const onSend = useCallback(async (messages = []) => {
-    try {
-      await ref.add(messages[0]);
-    } catch (error) {
-      console.log('dong51', error.message);
-    }
-  }, []);
-  if (!currUser?._user.displayName) {
-    return (
-      <SafeAreaView>
-        <TextInput
-          value={username}
-          onChangeText={setUsername}
-          label="Username"
-        />
-        <Button onPress={handleUpdate}>Update username</Button>
-        <Button onPress={logOut}>Logout</Button>
-      </SafeAreaView>
-    );
-  }
+  // const onSend = useCallback(async (messages = []) => {
+  //   try {
+  //     await ref.add(messages[0]);
+  //   } catch (error) {
+  //     console.log('dong51', error.message);
+  //   }
+  // }, []);
+  // if (!currUser?._user.displayName) {
+  //   return (
+  //     <SafeAreaView>
+  //       <TextInput
+  //         value={username}
+  //         onChangeText={setUsername}
+  //         label="Username"
+  //       />
+  //       <Button onPress={handleUpdate}>Update username</Button>
+  //       <Button onPress={logOut}>Logout</Button>
+  //     </SafeAreaView>
+  //   );
+  // }
   return (
     <GiftedChat
       messages={messages}
